bdd5ccb5200efb2f8b93a6adf31cde06c3e9a43f
greenlemonT <ms950219@naver.com>
2024-05-28 21:29:43 +0900

    init

---
+++ src/test/java/GLOW/OldHouseNewHouse/OldHouseNewHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package GLOW.OldHouseNewHouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OldHouseNewHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
